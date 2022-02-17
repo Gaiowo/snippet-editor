fb33fecad9066edf51d131cb6ccb849065e78495
Gai <23699120+Gaiowo@users.noreply.github.com>
2022-02-18 03:46:04 +0900

    feat: explicit copying

--- src/Organisms/content/SnippetExportForm.tsx
+++ src/Organisms/content/SnippetExportForm.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import Modal from "../../Atoms/Modal";
 import Button from "../../Atoms/Button";
 import { ListedSnippet } from "./snippet";
@@ -10,6 +10,8 @@ interface SnippetExportFormProps {
 }
 
 function SnippetExportForm(props: SnippetExportFormProps): JSX.Element {
+  const [copied, setCopied] = useState(false);
+
   const fileBody: { [key: string]: { prefix: string[]; body: string[]; description: string } } = {};
 
   // set fileBody on mount
@@ -25,17 +27,18 @@ function SnippetExportForm(props: SnippetExportFormProps): JSX.Element {
   }, []);
 
   const copyFileBody = (): void => {
-    navigator.clipboard.writeText(JSON.stringify(fileBody, null, 2)).then(() => props.close());
+    navigator.clipboard.writeText(JSON.stringify(fileBody, null, 2)).then(() => setCopied(true));
   };
 
   return (
     <Modal className="w-1/3" {...props} title="Exporting Snippets">
       <p className="mb-1 dark:text-slate-50">Snippets are ready to export.</p>
-      <div className="w-full">
+      <div className="flex w-full gap-2">
+        <span className="block ml-auto leading-9 dark:text-slate-50">{copied ? "Copied!" : ""}</span>
         <Button
           title="Copy to Clipboard"
           onClick={copyFileBody}
-          className="block px-2 ml-auto leading-9 bg-gray-300 h-9 hover:bg-gray-400 dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-slate-50"
+          className="block px-2 leading-9 bg-gray-300 h-9 hover:bg-gray-400 dark:bg-gray-600 dark:hover:bg-gray-500 dark:text-slate-50"
         />
       </div>
     </Modal>
